0a018f4b7d45942f5db8e757249859b2dbd107b2
Piotr Biernacki <piotrekbiernacki09@gmail.com>
2024-08-16 19:58:28 +0200

    Pierwszy commit

---
+++ lista/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Widoki dostępne dla niezalogowanego użytkownika
+    path('register/', views.register, name='register'),
+    path('info/', views.info, name='info'),
+
+    # Widoki dostępne dla zalogowanego użytkownika
+    path('', views.task_list, name='task_list'),
+    path('task/<int:pk>/', views.task_detail, name='task_detail'),
+    path('task/new/', views.task_create, name='task_create'),
+    path('task/<int:pk>/edit/', views.task_update, name='task_update'),
+    path('task/<int:pk>/delete/', views.task_delete, name='task_delete'),
+]
